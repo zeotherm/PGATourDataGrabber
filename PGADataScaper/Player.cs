5dc0b7c7bdbdb08893360938a9ac1e88feec4daa
Matt Ford <zeotherm@gmail.com>
2017-02-07 22:40:55 -0500

    Initial code check in

---
+++ PGADataScaper/Player.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PGADataScaper {
+    public class Player {
+        public string Last { get; private set; }
+        public string First { get; private set; }
+        public string ID { get; private set; }
+
+        public Player(string l, string f, string i) {
+            Last = l.Trim();
+            First = f.Trim();
+            ID = i;
+        }
+
+        public Player(string[] n, string i) {
+            Last = n[0].Trim();
+            First = n[1].Trim();
+            ID = i;
+        }
+
+        public string FileName {
+            get {
+                return String.Format("{0}_{1}.json", Last.Replace(' ', '_'), First.Replace(' ', '_'));
+            }
+        }
+
+        public string FullName {
+            get {
+                return String.Format("{0} {1}", First, Last);
+            }
+        }
+    }
+}
---
+++ PGADataScaper/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace PGADataScaper {
+    class Program {
+        static void Main(string[] args) {
+            XDocument playerList = XDocument.Load(@"C:\Users\Matt\Desktop\PlayerList.xml");
+
+            var players = playerList.Descendants("option").Where( e => !String.IsNullOrEmpty(e.Attribute("value").Value));
+
+            var player_ids = players.Select(p => new Player(p.Value.Split(','), p.Attribute("value").Value.Split('.')[1]));
+            var Desktop = @"C:\Users\Matt\Desktop\PGA Stats";
+            var path = Path.Combine(Desktop, DateTime.Now.ToString("yyyyMMdd"));
+            var di = Directory.CreateDirectory(path);
+            var wc = new System.Net.WebClient();
+            foreach (var player in player_ids) {
+                var webpath = String.Format(@"http://www.pgatour.com/data/players/{0}/2017stat.json", player.ID);
+                var file_path = Path.Combine(di.FullName, player.FileName);
+                Console.WriteLine( "Downloading data for player {0}", player.FullName);
+                try {
+                    var playerJSON = wc.DownloadString(webpath);
+                    using (var player_file = new StreamWriter(file_path)) {
+                        Console.WriteLine("Writing file {0}", file_path);
+                        player_file.WriteLine(playerJSON);
+                    }
+                } catch (WebException) {
+                    Console.WriteLine(String.Format("An error occured the program could not download player data: {0}", player.FullName));
+                    using (var error_file = new StreamWriter(Path.Combine(di.FullName, "error.log"), true)) {
+                        error_file.WriteLine(player.FullName);
+                    }
+                }
+            }
+        }
+    }
+}
